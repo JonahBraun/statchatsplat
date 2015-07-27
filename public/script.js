648d2534487f45d73f975462bb0ac3f5352f4153
Jonah Braun <Jonah@goodcoffee.ca>
2015-07-27 15:08:23 -0600

    use chat div

--- public/script.js
+++ public/script.js
@@ -1,10 +1,20 @@
-var chatSocket = new WebSocket("ws://localhost:8008/ws");
+"use strict";
 
-chatSocket.onopen = function (event) {
-	chatSocket.send("Example Test sent to the server");
-};
+$(document).ready(function(){
+	var chatSocket = new WebSocket("ws://localhost:8008/ws");
 
-chatSocket.onmessage = function (event) {
-	console.log(event.data);
-};
+	chatSocket.onopen = function (event) {
+		chatSocket.send("Example Test sent to the server");
+	};
+
+	chatSocket.onmessage = function (event) {
+		$('#chat').append(event.data+"\n");
+	};
+
+	$('#chatsend').keyup(function(event){
+		if(event.keyCode == 13){
+			chatSocket.send($('#chatsend').value);
+		}
+	});
+});
 
